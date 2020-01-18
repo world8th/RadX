b2464dea51490895e814cf9e049c40aeff5cf47b
helix.d <helixd.2k18@gmail.com>
2020-01-18 22:12:21 +0800

    add some extensions

--- shaders-sdk/include/driver.glsl
+++ shaders-sdk/include/driver.glsl
@@ -57,6 +57,11 @@
 #extension GL_EXT_shader_explicit_arithmetic_types_int16 : enable
 #extension GL_EXT_shader_explicit_arithmetic_types_int8 : enable
 
+// 
+#extension GL_EXT_shader_subgroup_extended_types_int8 : enable
+#extension GL_EXT_shader_subgroup_extended_types_int16 : enable
+#extension GL_EXT_shader_subgroup_extended_types_int64 : enable
+
 // subgroup operations
 #extension GL_ARB_shader_ballot : enable
 #extension GL_AMD_shader_ballot : enable
